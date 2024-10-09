b1f79211ec852348bcea75a4f632b10b3e7cd575
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-10-10 00:51:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.carousel').carousel();
+  autoplay() 
+  function autoplay() { 
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);           
+  }
+});
+
+// Page  Image Slider
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
