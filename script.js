dc175d1e7c4d9a6b62d080008e139d1db2a8d2f1
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-10-13 19:46:56 +0530

    About Us page completed

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('.main'),
-  smooth: true
+  el: document.querySelector(".main"),
+  smooth: true,
 });
+
+// Facts counter
+$('[data-toggle="counter-up"]').counterUp({
+  delay: 10,
+  time: 2000,
+});
+
+
+
+new WOW().init();
+
